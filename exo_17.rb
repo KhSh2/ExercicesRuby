8f50c936fb72ceaaff7a37f00891ce2446adaa2d
KhSh2 <86683150+KhSh2@users.noreply.github.com>
2021-07-07 10:35:24 +0200

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -8,3 +8,6 @@ if number <=25 and number>=1 :
     puts  " "*(number-i)+"#"*(2 * i + 1)
     end
 end
+else
+  puts "Vous avez été trop gourmand, réessayez de relancer en vérifiant que votre nombre est compris entre 1 et 25 !!"
+end
