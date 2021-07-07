38ce992bfa434affa5bc265d04324db4e07a84b2
KhSh2 <86683150+KhSh2@users.noreply.github.com>
2021-07-07 09:45:59 +0200

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -2,7 +2,9 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 puts ">"
 number = gets.chomp.to_i
-puts "Voici la pyramide :"
-number.times do |i|
-  puts  " "*(number-i)+"#"*(2 * i + 1)
+if number <=25 :
+    puts "Voici la pyramide :"
+    number.times do |i|
+    puts  " "*(number-i)+"#"*(2 * i + 1)
+    end
 end
